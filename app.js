5507eb04923d7a7b1c98d61589ad8f72e5dae176
Gaurav Khare <gauravkhare1073@gmail.com>
2024-01-17 13:24:29 +0530

    fix - app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-require("dotenv").config()
+require("dotenv").config();
+const MongoStore = require("connect-mongo");
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var hbs = require("hbs");
 // const serviceCategoryModel = require("./routes/serviceCategory");
-var path = require('path');
+var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 //
@@ -19,14 +21,18 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set('view engine', 'ejs');
-hbs.registerPartials("views/partials")
+app.set("view engine", "ejs");
+hbs.registerPartials("views/partials");
 //
 app.use(
   expressSession({
     resave: false,
     saveUninitialized: false,
     secret: "P_6112000_01",
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_DB_CONNECTION,
+      touchAfter: 24 * 3600, // time period in seconds
+    }),
   })
 );
 app.use(passport.initialize());
@@ -42,7 +48,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use('/static',express.static("public"))
+app.use("/static", express.static("public"));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -58,7 +64,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-
 
 module.exports = app;
